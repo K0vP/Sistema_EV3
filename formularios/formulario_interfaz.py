cd6d227de7422d10f5174febd1eca33eb1f8455c
Felipe Aguero <felipe.aguero08@inacapmail.cl>
2024-12-12 00:57:05 -0300

    crear datos api e interfaz

--- formularios/formulario_interfaz.py
+++ formularios/formulario_interfaz.py
@@ -1,15 +1,34 @@
 from auxiliares.constantes import visualizar
 from servicios.obtener_datos_api import ObtenerDatosApi
+from modelos.posts import Post
+from modelos.comments import Comment
+from servicios.crear_datos_api import CrearPostApi, CrearCommentApi
+
 def menu ():
     print('Bienvenido')
     realizar = input('Que desea relaizar:\n1: Consultar\n2: Crear\n3: Editar\n4: Eliminar\nIngrese el numero de la opcion: ')
     if realizar == '1':
         opcion_consultar = input('Seleccione una de las siguintes opciones:\n1: Post\n2: Comments\nIngrese el numero de la opcion: ')
-        if opcion_consultar in [visualizar]:
-            print (f'Selecciono la opcion {visualizar}\n Estos son los posts:')
-            ObtenerDatosApi()
+        if opcion_consultar in visualizar:
+            print (f'Selecciono la opcion {visualizar[opcion_consultar]}:\n')
+            ObtenerDatosApi(opcion_consultar)
     elif realizar == '2':
-        opcion_crear = input('Seleccione una de las siguintes opciones:\n1: Post\n2: Comments\nIngrese el numero de la opcion: ')
+        opcion_crear = input('Seleccione una de las siguintes opciones:\n1: Post\n2: Comments\n3: Volver al menu\nIngrese el numero de la opcion: ')
+        if opcion_crear == '1':
+            user_id = input('Ingresar el id del usuario: ')
+            title = input('ingresar el titulo del post: ')
+            body = input('ingresar el cuerpo del post: ')
+            post = Post(user_id, None, title, body)
+            CrearPostApi(post)
+        elif opcion_crear == '2':
+            post_id = input('ingresar el id del post: ')
+            name = input('Ingrese el nombre del autor del comment: ')
+            email = input('Ingrese el email del autor del comment: ')
+            body = input('ingresar el cuerpo del comment: ')
+            comment = Comment(post_id, id, name, email, body)
+            CrearCommentApi(comment)
+        elif opcion_crear == '3':
+            menu()
     elif realizar == '3':
         opcion_editar = input('Seleccione una de las siguintes opciones:\n1: Post\n2: Comments\nIngrese el numero de la opcion: ')
     elif realizar == '4':
--- servicios/obtener_datos_api.py
+++ servicios/obtener_datos_api.py
@@ -1,11 +1,11 @@
 import requests as req
 from servicios.servicio_url import UrlCompleta
 
-def ObtenerDatosApi() :
-    url = UrlCompleta()
+def ObtenerDatosApi(opcion) :
+    url = UrlCompleta(opcion)
     try:
         respuesta = req.get(url)
         if respuesta.status_code == 200:
-            return respuesta.json()
+            print(respuesta.json())
     except:
         print(f"Error al obtener los datos, detalles:",respuesta.text)
